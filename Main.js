32a41b88cea162043858f9b692966111a9678094
Reza Saker <saker.reza001@hotmail.com>
2020-12-30 05:19:47 -0600

    Added design for team info page

--- Main.js
+++ Main.js
@@ -9,21 +9,90 @@ class Main
 		//Show the home section and hide other sections of the content_body
 		this.displaySection("home", "Home");
 
-		//Add EventListeners to menu buttons
-		home_btn.addEventListener("click", function() { this.displaySection("home", "Home"); }.bind(this));
-		basic_info_btn.addEventListener("click", function() { this.displaySection("basic_info", "Basic Information"); }.bind(this));
-		team_info_btn.addEventListener("click", function() { this.displaySection("team_info", "Team Information"); }.bind(this));
-		events_info_btn.addEventListener("click", function() { this.displaySection("events_info", "Events Information"); }.bind(this));
-		social_media_btn.addEventListener("click", function() { this.displaySection("social_media_info", "Social Media Links"); }.bind(this));
-		messages_btn.addEventListener("click", function() { this.displaySection("messages", "Messages"); }.bind(this));
-		contact_us_btn.addEventListener("click", function() { this.displaySection("contact_us", "Contact Us"); }.bind(this));
-		change_password_btn.addEventListener("click", function() { this.displaySection("change_password", "Change Password"); }.bind(this));
+		//Home button
+		home_btn.addEventListener("click", function() { 
+
+			this.displaySection("home", "Home"); 
+
+		}.bind(this));
+
+
+		//Basic info button
+		basic_info_btn.addEventListener("click", function() { 
+
+			this.displaySection("basic_info", "Basic Information"); 
+
+		}.bind(this));
+
+
+		//Team info button
+		team_info_btn.addEventListener("click", function() { 
+
+			this.displaySection("team_info", "Team Information"); 
+
+		}.bind(this));
+
+
+		//Events info button
+		events_info_btn.addEventListener("click", function() { 
+
+			this.displaySection("events_info", "Events Information"); 
+
+		}.bind(this));
+
+		//Social media info button
+		social_media_info_btn.addEventListener("click", function() { 
+
+			this.displaySection("social_media_info", "Social Media Links"); 
+
+		}.bind(this));
+
+		//Messages button
+		messages_btn.addEventListener("click", function() { 
+
+			this.displaySection("messages", "Messages"); 
+
+		}.bind(this));
+
+		//Contact us button
+		contact_us_btn.addEventListener("click", function() { 
+
+			this.displaySection("contact_us", "Contact Us"); 
+
+		}.bind(this));
+
+		//Change password button
+		change_password_btn.addEventListener("click", function() { 
+
+			this.displaySection("change_password", "Change Password"); 
+
+		}.bind(this));
+
+		//Update picture buttons
+		let memberInfoBtns = team_info.getElementsByClassName("update_picture_btn");
+
+		for(let btn of memberInfoBtns)
+		{
+			btn.addEventListener("click", function() { 
+
+				this.displayPopupForm(change_picture_form); 
+
+			}.bind(this));
+		}
+
+
+		//Close change picture button
+		close_change_picture_form.addEventListener("click", function() { 
+
+			this.closePopupForm(event, change_picture_form); 
+
+		}.bind(this));
 	}
 
 
 	//////////////////////////////////////////////////////////////////////////////
 	//
-	//Event Handler Functions
+	//EVENT HANDLER FUNCTIONS
 	//
 	/////////////////////////////////////////////////////////////////////////////
 
@@ -34,7 +103,7 @@ class Main
 	 */
 	displaySection(id, title)
 	{
-		//Show the basic info section and hide other sections of the content_body
+		//Show the selected section and hide other sections of the content_body
 		let contentSections = content_body.getElementsByTagName("div");
 		
 		for (let div of contentSections) 
@@ -44,13 +113,78 @@ class Main
 				div.classList.remove("hidden");
 				div.classList.add("visible");
 			}
-			else
+			else if(!div.classList.contains("member_info") && !div.classList.contains("event_box"))
 			{
+
 				div.classList.add("hidden");
 			}
 		}
 
 		//Display the title of selected menu in the content header
 		content_header.getElementsByTagName("h2")[0].textContent = title;
+
+		//Mark the currently selected menu in menu bar and remove mark from previous menu button
+		let menuButtons = menu_buttons.getElementsByTagName("li");
+		let currentButtonId = id + "_btn";
+
+		for(let li of menuButtons)
+		{
+			if(li.id == currentButtonId)
+			{
+				li.classList.add("active");
+			}
+			else if(li.classList.contains("active"))
+			{
+				li.classList.remove("active");
+			}
+		} 
 	}
+
+
+
+	//////////////////////////////////////////////////////////////////////////////
+	//
+	//FUNCTIONS FOR HANDLING POPUP FORMS
+	//
+	/////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 *Function to display a popup form
+	 *@param { form object } form - Id of the form to be displayed
+	 */
+	displayPopupForm(form)
+	{
+		//Display the form
+		if(form.classList.contains("hidden"))
+		{
+			form.classList.remove("hidden");
+			form.classList.add("visible");
+		}
+
+		//Blurr out the background
+		if(!content_body.classList.contains("blurry"))
+		{
+			content_body.classList.add("blurry");
+		}
+	}
+
+	closePopupForm(event, form)
+	{
+		//Prevent any form submission due to the click of cancel button
+		event.preventDefault();
+
+		//Hide the form
+		if(form.classList.contains("visible"))
+		{
+			form.classList.remove("visible");
+			form.classList.add("hidden");
+		}
+
+		//Clear the background blurryness
+		if(content_body.classList.contains("blurry"))
+		{
+			content_body.classList.remove("blurry");
+		}
+	}
+	
 }
