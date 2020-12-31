978a0a0786c9e28319eecf2b97507d5c29ad0712
Reza Saker <saker.reza001@hotmail.com>
2020-12-31 07:11:59 -0600

    Added login functionality

---
+++ login/Authentication.js
@@ -0,0 +1,100 @@
+/**
+ *Class for handling all login related functionalities
+ */
+class Authentication 
+{
+	constructor() 
+	{
+		//Add eventListeners
+		username.addEventListener("input", function() { this.removeElementHighlight(username); }.bind(this));
+		password.addEventListener("input", function() { this.removeElementHighlight(password); }.bind(this));
+		submit_btn.addEventListener("click", this.authenticateUser.bind(this));
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+	//
+	//FUNCTIONS FOR AUTHENTICATING USER
+	//
+	////////////////////////////////////////////////////////////////////////////
+
+	authenticateUser(event) 
+	{
+		//Prevent default form submission
+		event.preventDefault();
+		
+		//Remove any previous status
+		message.textContent = null;
+
+		//Send request to server to verify the login credentials
+		let xhr = new XMLHttpRequest();
+		let formData = new FormData();
+
+		formData.append("username", username.value);
+		formData.append("password", password.value);
+		formData.append("access_key", "1234");
+
+		xhr.open("POST", "http://localhost/uwimpact_cms_api/authenticateUser.php", true);
+		xhr.send(formData);
+
+		this.xhrRequestHandler(xhr);
+	}
+
+	xhrRequestHandler(xhr) 
+	{
+		xhr.onload = function()
+		{
+			if(xhr.status >= 200 && xhr.status < 300) 
+			{
+				if(xhr.response != "ERROR")
+				{
+					//user verified as authentic,
+					//Redirect to the CMS homepage
+					window.location = "../index.html";
+				}
+				else
+				{
+					this.showError();
+				}
+			}
+			else //if the remote server sent an error
+			{
+				this.showError();
+			}
+		}.bind(this);
+	}
+
+	showError()
+	{
+		//Login credentials are wrong,
+		//Show error message and highlight username and pasword fields
+		message.textContent = "Invalid username or password. If you think you forgot your password, contact UW Impact team.";
+		this.highlightElement(username);
+		this.highlightElement(password);
+	}
+
+	/////////////////////////////////////////////////////////////////////////////
+	//
+	//FUNCTIONS FOR MANIPULATING STYLE
+	//
+	/////////////////////////////////////////////////////////////////////////////
+
+	highlightElement(element)
+	{
+		if(!element.classList.contains("highlight"))
+		{
+			element.classList.add("highlight");
+		}
+	}
+
+	removeElementHighlight(element)
+	{
+		if(element.classList.contains("highlight"))
+		{
+			element.classList.remove("highlight");
+		}
+
+		//Remove any previous status
+		message.textContent = null;
+	}
+
+}
